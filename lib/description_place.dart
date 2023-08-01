fe65153569d591a012d1e658ab38ce6cc83ca262
Mario Roberto <mrg@treming.com>
2023-07-31 20:09:42 -0600

    Flutter Widgets: Column

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -2,55 +2,85 @@ import 'package:flutter/material.dart';
 
 class DescriptionPlace extends StatelessWidget {
 
-  //variables declaradas
   String namePlace;
-  int starts;
+  int stars;
   String descriptionPlace;
 
-  // Constructor
-  DescriptionPlace(this.namePlace, this.starts, this.descriptionPlace);
+  DescriptionPlace(this.namePlace, this.stars, this.descriptionPlace, {super.key});
 
 
-  String descriptionDummy =
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
-      "when an unknown printer took a galley of type and scrambled it to make a type "
-      "specimen book. \n\n It has survived not only five centuries, but also the leap "
-      "into electronic."
-      "This is a new paragraph in the description with a line break."
-      "And this is another paragraph with more text.";
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    final star = Container(
-      margin: const EdgeInsets.only(top: 323.0, right: 3.0),
+    final star_half = Container (
+      margin: const EdgeInsets.only(
+          top: 323.0,
+          right: 3.0
+      ),
+
       child: const Icon(
-        Icons.star,
-        color: Color(0xFFf2c611),
+        Icons.star_half,
+        color:  Color(0xFFf2C611),
       ),
     );
 
-    final title_stars = Row(
+    final star_border = Container (
+      margin: const EdgeInsets.only(
+          top: 323.0,
+          right: 3.0
+      ),
+
+      child: const Icon(
+        Icons.star_border,
+        color:  Color(0xFFf2C611),
+      ),
+    );
+
+    final star = Container (
+      margin: const EdgeInsets.only(
+          top: 323.0,
+          right: 3.0
+      ),
+
+      child: const Icon(
+        Icons.star,
+        color:  Color(0xFFf2C611),
+      ),
+    );
+
+    final title_stars = Row (
       children: <Widget>[
-        Container(
-          margin: const EdgeInsets.only(top: 320.0, left: 20.0, right: 20.0),
-          child: const Text(
-            "Duwili Ella",
-            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w900),
+        Container (
+          margin: const EdgeInsets.only(
+              top: 320.0,
+              left: 20.0,
+              right: 20.0
+          ),
+
+          child: Text(
+            namePlace,
+            style: const TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.w900
+            ),
             textAlign: TextAlign.left,
           ),
+
         ),
+
         Row(
           children: <Widget>[
             star,
             star,
             star,
             star,
-            star,
+            star_half
           ],
-        ),
+        )
+
+
       ],
     );
 
@@ -73,8 +103,13 @@ class DescriptionPlace extends StatelessWidget {
     );
 
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [title_stars, description],
+      children: <Widget>[
+        title_stars,
+        description
+      ],
     );
+
+
   }
+
 }
