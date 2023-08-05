0d159c8d8ee207dc5d0ba6091c179c178a7a3c74
Mario Roberto <mrg@treming.com>
2023-08-05 15:15:18 -0600

    Se agregaron img a los assets y titulo a la pantalla principal en un string asi como se mejoro la barra de estado en android"

--- android/app/src/main/kotlin/com/platzitrips/app/platzi_trips_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/platzitrips/app/platzi_trips_app/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.platzitrips.app.platzi_trips_app
 
+import android.os.Build
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.annotation.RequiresApi
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.statusBarColor = android.graphics.Color.TRANSPARENT
+        // Aquí puedes agregar cualquier código que desees ejecutar al crear la actividad
+        //Lo corregi en android
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
                 const ReviewList(),
               ],
             ),
-            GradientBack(),
+            GradientBack("Popular"),
           ],
         ),
       ),
