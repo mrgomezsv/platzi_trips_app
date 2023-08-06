a4f3adf7edb70b6e19b99f9b09dd6930d6d14817
Mario Roberto <mrg@treming.com>
2023-08-05 21:52:33 -0600

    se agrego estados al widget floatin action button green para cambiar entre favorito o no

--- lib/floating_action_button_green.dart
+++ lib/floating_action_button_green.dart
@@ -6,31 +6,36 @@ class FloatingActionButtonGreen extends StatefulWidget{
     // TODO: implement createState
     return _FloatingActionButtonGreen();
   }
-
 }
 
 class _FloatingActionButtonGreen extends State<FloatingActionButtonGreen>{
+  bool idPressed = false;
 
   void onPressedFav(){ //Creamos el metodo
+
+    setState(() {
+      idPressed = ! idPressed;
       ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text("Agresgaste a tus Favoritos"),
+          SnackBar(
+            //content: Text("Agresgaste a tus Favoritos"),
+            content: Text(((idPressed)?"Agregaste a":"Eliminaste de")+" favoritos"),
           )
       );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
     return FloatingActionButton(
       backgroundColor: Color(0xFF11DA53),
       mini: true,
       tooltip: "Fav",
       onPressed: onPressedFav, //Asignamos el metodo
-      child: const Icon(
-        Icons.favorite_border
+      child: Icon(
+          (idPressed)? Icons.favorite : Icons.favorite_border,
       ),
     );
   }
-
 }
